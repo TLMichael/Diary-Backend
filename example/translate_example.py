c616adcb6930d5d14e934f9a9c60714f99df0ab7
TLMichael <tlmichael@nuaa.edu.cn>
2018-11-13 16:40:52 +0800

    fix

--- example/translate_example.py
+++ example/translate_example.py
@@ -5,3 +5,13 @@ translator = Translator()
 text = '我爱你'
 res = translator.translate(text)
 print(text, res)
+
+
+print('调用translate命令')
+
+import os
+
+command = 'proxychains translate ' + text
+
+res = os.system(command)
+print(res)
